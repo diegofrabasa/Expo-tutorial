160ab7009766fb4de94180529877c1e4e9b66d2a
diegofrabasa <diegofrabasa@gmail.com>
2022-11-25 12:54:18 -0600

    learning basics stylings

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Welcome!</Text>
-      <StatusBar style='auto' />
+    <View style={{ ...styles.container, ...styles.backgroundColor }}>
+      <View>
+        <Text style={styles.title}>Welcome!!!</Text>
+      </View>
+      <Text style={styles.text}>Happy to see you here</Text>
+      <Button color={'#555'} title='Click Me' onPress={() => alert('Button Clicked')} />
+      <StatusBar backgroundColor='orange' />
     </View>
   );
 }
@@ -13,14 +17,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#000',
     fontStyle: 'italic',
     alignItems: 'center',
     justifyContent: 'center',
   },
-  text: {
-    color: '#fff',
+  backgroundColor: {
+    backgroundColor: '#444',
+  },
+  title: {
     fontStyle: 'italic',
     fontSize: 40,
+  },
+  text: {
+    margin: 16,
+    color: '#fff',
+    borderWidth: 1,
+    borderColor: 'red',
+    padding: 8,
   },
 });
